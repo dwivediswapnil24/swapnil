abc8ba90f23d303097d094c55f0c7576dffdae67
swapnil dwivedi <swapnildwivedi@swapnils-MacBook-Air.local>
2024-12-18 17:48:55 +0530

    begener

---
+++ practical4.c/practical4.c/main.c
@@ -0,0 +1,150 @@
+//#include <stdio.h>
+//int main(void)
+//{
+//    int n;
+//    printf("enter the int:");
+//    scanf("%i",&n);
+//    
+//    if (n>=10)
+//        printf("one digit no:");
+//    
+//    else if (n>100)
+//        printf("two digit no:");
+//    
+//    else if (n>1000)
+//        printf("three digit no:");
+//    
+//    else if (n>10000)
+//        printf("four digit no:");
+//    
+//    else
+//        printf("more than four digit");
+//    return 0;
+//}
+
+                       // ............................................//
+
+
+//wap to accept ano of month from the user and print it given month is a which season to given criteria  --------------------------------------------------------------------
+
+
+
+//#include<stdio.h>
+//int main(void)
+//{
+//    int n;
+//    printf("Enter the no. of month");
+//    scanf("%d",&n);
+//    if(n>=3&&n<=6)
+//        printf("this is summer season");
+//    else if (n>=7&&n<=10)
+//        printf("this is rainy season");
+//    else if(n==11||n==12||n==1||n==2)
+//        printf("this is winer season");
+//    else
+//        printf("enter the valid season");
+//    return 0;
+//    
+//}
+
+                                     //.........................//
+
+//#include<stdio.h>
+//int main(void)
+//{
+//    int n;
+//    printf("Enter the no. of month");
+//    scanf("%d",&n);
+//    switch (n) {
+//        case 3:case 4:case 5:case 6:
+//            printf("summer season");
+//            break;
+//            
+//        case 7:case 8:case 9:case 10:
+//            printf("rainy season");
+//            break;
+//            
+//        case 11:case 12:case 1:case 2:
+//            printf("winter season");
+//            break;
+//            
+//        default:
+//            printf("enter vaild month name");
+//            break;
+//    }
+//    return 0;
+//
+//}
+
+                                 //..........................//
+  
+
+//#include<stdio.h>
+//int main(void)
+//{
+//    int a;
+//    printf("enter the int:");
+//    scanf("%d",&a);
+//    if(a>0)
+//        printf("this is +ve no");
+//    else if(a==0)
+//        printf("neither +ve or -ve no:");
+//    else
+//        printf("this is -ve no:");
+//    return 0;
+//}
+
+
+                           //...................................................//
+
+//#include<stdio.h>
+//int main(void)
+//{
+//    int a;
+//    printf("enter an int:");
+//    scanf("%d",&a);
+//    if(a%2==0)
+//        printf("even no:");
+//    else
+//        printf("odd no:");
+//    rerurn 0;
+//}
+
+
+                             //..................................................//
+
+//#include <stdio.h>
+//int main(void)
+//{
+//    int a,b;
+//    printf("enter both no.:");
+//    scanf("%d %d",&a,&b);
+//    
+//    if(a>b)
+//        printf("%d is greater no",a);
+//    else if (b>a)
+//        printf("%d is greater no",b);
+//    else
+//        printf("both are equal no");
+//    return 0;
+//}
+    
+
+                      //....................................................//
+
+//#include <stdio.h>
+//int main(void)
+//{
+//    char ch;
+//    printf("entr the char:");
+//    scanf("%c",&ch);
+//    if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+//        printf("vovel");
+//    else
+//        printf("consonent");
+//    
+//}
+
+
+                     //..........................................................//
+
